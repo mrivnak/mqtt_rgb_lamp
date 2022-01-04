2fb0800b17ea8f614e57b75a0cfcd2a5340d6465
Michael Rivnak <rivnakm1@gmail.com>
2022-01-04 14:45:17 -0500

    Working software prototype

---
+++ lamp.py
@@ -0,0 +1,85 @@
+import json
+from gpiozero import PWMLED
+from typing import Dict
+from dataclasses import dataclass
+
+
+@dataclass
+class RGBColor:
+    r: int
+    g: int
+    b: int
+
+    @property
+    def hex(self):
+        return "#%02x%02x%02x" % (self.r, self.g, self.b)
+
+
+class Lamp:
+    STATE_ON: str = "ON"
+    STATE_OFF: str = "OFF"
+
+    def __init__(self):
+        self.brightness: int = 100
+        self.color_mode: str = "color_temp"
+        self.color_rgb: RGBColor = RGBColor(r=255, g=255, b=255)
+        self.color_temp: int = 153
+        self._on: bool = True
+
+    @property
+    def on(self) -> str:
+        return self.STATE_ON if self._on else self.STATE_OFF
+
+    @on.setter
+    def on(self, state: str) -> None:
+        if state == self.STATE_ON:
+            self._on = True
+        elif state == self.STATE_OFF:
+            self._on = False
+
+    def update(self) -> None:
+        # TODO: update actual hardware to match settings
+        pass
+
+    def read_json(self, json_data: str) -> None:
+        data = json.loads(json_data)
+
+        if on := data.get("state"):
+            self.on = on
+        # NOTE: Color mode never actually gets sent
+        # if color_mode := data.get("color_mode"):
+        #     self.color_mode = color_mode
+        if brightness := data.get("brightness"):
+            self.brightness = brightness
+        if color_temp := data.get("color_temp"):
+            self.color_temp = color_temp
+            self.color_mode = "color_temp"
+        elif color := data.get("color"):
+            self.color_rgb = RGBColor(r=color["r"], g=color["g"], b=color["b"])
+            self.color_mode = "rgb"
+
+        self.update()
+
+    def get_json(self) -> str:
+        output = {
+            "state": self.on,
+            "color_mode": self.color_mode,
+            "brightness": self.brightness,
+            "color_temp": self.color_temp,
+            "color": {
+                "r": self.color_rgb.r,
+                "g": self.color_rgb.g,
+                "b": self.color_rgb.b,
+            },
+        }
+        return json.dumps(output)
+
+    def __str__(self):
+        color_string = (
+            f"Color temp: {self.color_temp}"
+            if self.color_mode == "color_temp"
+            else f"RGB: {self.color_rgb.hex}"
+        )
+        return (
+            f"Lamp: [ On: {self._on}, Brightness: {self.brightness}, {color_string} ]"
+        )
---
+++ mqtt_rgb.py
@@ -0,0 +1,44 @@
+import paho.mqtt.client as mqtt
+import json
+from lamp import Lamp
+
+BASE_TOPIC = "lamp/cat"
+STATE_TOPIC = f"{BASE_TOPIC}"
+COMMAND_TOPIC = f"{BASE_TOPIC}/set"
+
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("lamp/cat/#")
+
+def on_message(client, userdata, msg):
+
+    if msg.topic == COMMAND_TOPIC:
+        print(f"{msg.topic} {msg.payload.decode()}")
+        client.lamp.read_json(msg.payload.decode())
+
+        output = client.lamp.get_json().encode('utf-8')
+        print(f"payload in: {msg.payload}")
+        print(f"payload out: {output}")
+        client.publish(STATE_TOPIC, payload=output)
+    
+
+    print(client.lamp)
+
+
+def main():
+    client = mqtt.Client()
+    client.lamp = Lamp()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect("192.168.1.7", 1883, 60)
+
+    client.loop_forever()
+
+
+if __name__ == "__main__":
+    main()
